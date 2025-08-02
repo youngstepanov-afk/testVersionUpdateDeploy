6b2874cdc07e2299a8db8e09aec6df679addc589
Nikita <youngstepanov@gmail.com>
2025-08-02 21:41:13 +0300

    V.0.8

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 ReactModal.setAppElement('#root');
 
-const APP_VERSION = '0.1';
+const APP_VERSION = '0.2';
 
 function App() {
   const [showUpdateModal, setShowUpdateModal] = useState(false);
